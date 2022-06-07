9cb6fe10103da81c938e8ba06b9d9bb73a621599
shelen ells <shelen.ells@gmail.com>
2022-06-07 15:56:15 -0700

    second hello

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,3 +1,19 @@
+function createCard(name, description, pictureUrl, starts, ends) {
+    return `
+      <div class="card g-col-2 gap-3 shadow p-3 mb-5 bg-body rounded col-sm">
+        <img src="${pictureUrl}" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">${name}</h5>
+          <p class="card-text">${description}</p>
+        </div>
+            <div class="card-footer text-muted">
+            ${starts} - ${ends}
+            </div>
+      </div>
+    `;
+}
+
+
 window.addEventListener('DOMContentLoaded', async () => {
 
     const url = 'http://localhost:8000/api/conferences/';
@@ -9,23 +25,55 @@ window.addEventListener('DOMContentLoaded', async () => {
         } else {
             const data = await response.json();
 
-            const conference = data.conferences[0];
-            const nameTag = document.querySelector('.card-title');
-            nameTag.innerHTML = conference.name;
-            // const imageTag = document.querySelector('.card-img-top');
-            // imageTag.src = details.conference.location.picture_url;
 
-
-            const detailUrl = `http://localhost:8000${conference.href}`;
-            const detailResponse = await fetch(detailUrl);
-            if (detailResponse.ok) {
-                const details = await detailResponse.json();
-                const description = details.conference.description;
-                const detailsTag = document.querySelector('.card-text');
-                detailsTag.innerHTML = description;
+            for (let conference of data.conferences) {
+                const detailUrl = `http://localhost:8000${conference.href}`;
+                const detailResponse = await fetch(detailUrl);
+                if (detailResponse.ok) {
+                    const details = await detailResponse.json();
+                    const title = details.conference.name;
+                    const startDate = new Date(details.conference.starts).toDateString();
+                    const endDate = new Date(details.conference.ends).toDateString();
+                    const description = details.conference.description;
+                    const pictureUrl = details.conference.location.picture_url;
+                    const html = createCard(title, description, pictureUrl, startDate, endDate);
+                    const column = document.querySelector('.row');
+                    column.innerHTML += html;
+                }
             }
         }
     } catch (e) {
         console.error('error', e);
     }
 });
+
+
+// const html = createCard(title, description, pictureUrl);
+                    // const column = document.querySelector('.col');
+                    // column.innerHTML += html;
+    //                 const data = await response.json();
+
+    //   for (let conference of data.conferences) {
+    //     const detailUrl = `http://localhost:8000${conference.href}`;
+    //     const detailResponse = await fetch(detailUrl);
+    //     if (detailResponse.ok) {
+    //       const details = await detailResponse.json();
+    //       const title = details.conference.title;
+    //       const description = details.conference.description;
+    //       const pictureUrl = details.conference.location.picture_url;
+    //       const html = createCard(title, description, pictureUrl);
+    //       console.log(html);
+
+
+    // const details = await detailResponse.json();
+    // const description = details.conference.description;
+    // const title = details.conference.title;
+    // const pictureUrl = details.conference.location.picture_url
+    // // const detailsTag = document.querySelector('.card-text');
+    // // detailsTag.innerHTML = description;
+    // const imageTag = document.querySelector('.card-img-top');
+    // imageTag.src = details.conference.location.picture_url;
+    // const html = createCard(title, description, pictureUrl);
+    // const column = document.querySelector('.col');
+    // column.innerHTML = html;
+    // console.log("DATA:", details);
